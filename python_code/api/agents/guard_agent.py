ca7bddb9b67811eb192c9c76292d45047bfe0f44
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-04-27 17:02:25 -0300

    feat(guard_agent): adicionar lógica de resposta e pós-processamento no GuardAgent

--- python_code/api/agents/guard_agent.py
+++ python_code/api/agents/guard_agent.py
@@ -7,7 +7,6 @@ from copy import deepcopy
 
 dotenv.load_dotenv()  # Carregar variáveis de ambiente
 
-
 class GuardAgent():
     def __init__(self):
         # Inicializar o cliente OpenAI com a chave de API e a URL base
@@ -45,3 +44,43 @@ class GuardAgent():
             "message": deixe a mensagem vazia (empty strings) se "decision" for "allowed", caso contrário, escreva "Desculpe, não posso ajudar com isso. Posso te ajudar com seu pedido?".
             }
         """
+
+        # Adicionar o prompt do sistema e as mensagens anteriores (últimas 3 mensagens)
+        input_messages = [
+            {"role": "system", "content": system_prompt}] + messages[-3:]
+
+        # Resposta do chatbot
+        chatbot_output = get_chatbot_response(
+            self.client, self.model_name, input_messages)
+        # Resposta ao usuário:
+        output = self.postprocess(chatbot_output)
+
+        return output
+
+    def postprocess(self, output):
+        # Docstring
+        '''
+        Função para pós-processamento da resposta do chatbot.
+        Ela converte a resposta do chatbot em um dicionário estruturado, extraindo as informações relevantes.
+        A função também garante que a resposta esteja no formato correto, com as chaves e valores apropriados.
+
+        Parameters:
+        self (GuardAgent): Instância da classe GuardAgent.
+        output (str): A resposta do chatbot em formato JSON.
+
+        Returns:
+        dict: Um dicionário estruturado com as informações relevantes da resposta do chatbot.
+        '''
+        output = json.loads(output)
+
+        dict_output = {
+            "role": "assistant",  # papel: assistente da mensagem
+            "content": output["message"],
+            "memory": {  # se lembrar dos passos (anteriores, atual e próximos)
+                "agent": "guard_agent",  # agente atual
+                # decisão do agente guard
+                "guard_decision": output["decision"],
+            }
+        }
+
+        return dict_output
